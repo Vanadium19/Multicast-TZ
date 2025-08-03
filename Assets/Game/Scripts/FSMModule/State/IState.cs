1bf271506fbb98d484302e32f3bcb076acef36e6
Vladislav <GimliVlad@list.ru>
2025-08-03 15:47:58 +0400

    Add fsm

---
+++ Assets/Game/Scripts/FSMModule/State/IState.cs
@@ -0,0 +1,9 @@
+﻿namespace Game.Modules.FSM
+{
+    public interface IState
+    {
+        public void OnEnter();
+        public void OnUpdate(float deltaTime);
+        public void OnExit();
+    }
+}
---
+++ Assets/Game/Scripts/FSMModule/StateMachine/IAutoStateMachine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game.Modules.FSM
+{
+    public interface IAutoStateMachine<TKey> : IStateMachine<TKey>
+    {
+        public event Action<IStateTransition<TKey>> OnTransitionAdded;
+        public event Action<IStateTransition<TKey>> OnTransitionRemoved;
+
+        public int TransitionCount { get; }
+        public IEnumerable<(TKey, TKey)> Transitions { get; }
+
+        public bool AddTransition(IStateTransition<TKey> transition);
+        public bool AddTransition(TKey from, TKey to, Func<bool> condition);
+
+        public bool RemoveTransition(IStateTransition<TKey> transition);
+        public bool RemoveTransition(TKey from, TKey to);
+
+        public bool ContainsTransition(IStateTransition<TKey> transition);
+        public bool ContainsTransition(TKey from, TKey to);
+    }
+}
